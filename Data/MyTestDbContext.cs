117b3a8d7efa05f684d1944a8ba50d6e96e3a133
Cetin OGUT <cetinogut.gm@gmail.com>
2022-04-03 14:18:56 +0300

    added postgres + new db context

---
+++ Data/MyTestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using reactnet.Models;
+
+namespace reactnet.Data;
+
+public class MyTestDbContext : DbContext
+{
+    public MyTestDbContext(DbContextOptions<MyTestDbContext> options): base(options)
+    {
+
+    }
+    public DbSet<Note> Notes { get; set; }
+}
